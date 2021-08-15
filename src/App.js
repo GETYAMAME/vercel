5cd4c5e0273ce82684c493ae857001786f200b31
GETYAMAME <pon.mikan10@gmail.com>
2021-08-16 00:59:05 +0900

    入門2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React, { Component }  from 'react';
+import  { List }  from './List';
+import React from 'react';
+import {useState} from 'react';
 
 function App() {
+  const [description,setDescription] = useState('クリック前の表示');
+
+  const changeDescription = () => {
+    setDescription('変えたよ')
+  }
+
   return (
-    <div>コード修正頑張るぞ
+    <div>
+      { description }
+      <List title ="取扱い言語一覧"/>
+      <button onClick={changeDescription}>ボタン</button>
     </div>
   );
 }
